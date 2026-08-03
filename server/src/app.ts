7ccdef4900c8916f25bd4ed1c34942c7938eedc7
Serhiy <125665311+Serhiy8@users.noreply.github.com>
2026-08-03 08:29:13 +0300

    Update app.ts

--- server/src/app.ts
+++ server/src/app.ts
@@ -16,7 +16,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://ai-task-scheduler-8184.onrender.com/",
+      "https://ai-task-scheduler-pink.vercel.app",
     ],
     credentials: true,
   })
